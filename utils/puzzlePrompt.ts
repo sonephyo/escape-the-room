91dfb423668ec3f41feca282e405852b590f2584
Soney <sonephyo7777777@gmail.com>
2025-09-20 05:36:07 -0400

    update: added solution Prompt

--- utils/puzzlePrompt.ts
+++ utils/puzzlePrompt.ts
@@ -3,4 +3,8 @@ Generate a puzzle based on the given character data.
 Return a JSON object with a Puzzle field containing the puzzle text.
 `;
 
-export { puzzlePrompt };
+const solutionPrompt = `
+Generate a solution for the given puzzle. Should be only one word or short phrase.
+`;
+
+export { puzzlePrompt, solutionPrompt };
